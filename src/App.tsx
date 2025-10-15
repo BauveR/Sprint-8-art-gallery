bf1e3c00f418f4797fc6508639b5e5ca8d8146c9
BauveR <rick.bauve@gmail.com>
2025-10-16 00:05:11 +0200

    branch from main  to work in frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContextFirebase";
 import { CartProvider } from "./context/CartContext";
-import { PublicRoutes } from "./routes/PublicRoutes";
-import { ProtectedRoutes } from "./routes/ProtectedRoutes";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
+
+// Páginas públicas
+import PublicHome from "./pages/PublicHome";
+import LoginPage from "./pages/LoginPage";
+import UnauthorizedPage from "./pages/UnauthorizedPage";
+import ShopPage from "./pages/ShopPage";
+import CartPage from "./pages/CartPage";
+import ObraDetailPage from "./pages/ObraDetailPage";
+import MyOrdersPage from "./pages/MyOrdersPage";
+
+// Páginas protegidas
+import DashboardPage from "./pages/DashboardPage";
+import CheckoutPage from "./pages/CheckoutPage";
 
 /**
  * App principal con configuración de proveedores y enrutamiento
@@ -14,10 +26,31 @@ export default function App() {
         <BrowserRouter>
           <Routes>
             {/* Rutas públicas */}
-            <PublicRoutes />
+            <Route path="/" element={<PublicHome />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/unauthorized" element={<UnauthorizedPage />} />
+            <Route path="/shop" element={<ShopPage />} />
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="/obra/:id" element={<ObraDetailPage />} />
+            <Route path="/my-orders" element={<MyOrdersPage />} />
 
             {/* Rutas protegidas */}
-            <ProtectedRoutes />
+            <Route
+              path="/checkout"
+              element={
+                <ProtectedRoute allowedRoles={["admin", "user"]}>
+                  <CheckoutPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/dashboard"
+              element={
+                <ProtectedRoute allowedRoles={["admin"]}>
+                  <DashboardPage />
+                </ProtectedRoute>
+              }
+            />
 
             {/* Ruta por defecto - Redirigir al home */}
             <Route path="*" element={<Navigate to="/" replace />} />
